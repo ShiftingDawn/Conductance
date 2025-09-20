c11e693d1d6f14601d0db1296ceffab29e3965fc
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-09-20 09:24:21 +0200

    Fix group tags having wrong contents

--- src/main/java/conductance/lib/pack/server/TagGenerationHandler.java
+++ src/main/java/conductance/lib/pack/server/TagGenerationHandler.java
@@ -38,35 +38,29 @@ final class TagGenerationHandler {
 
 	private static void addItemEntriesToTagMap(final Map<ResourceLocation, List<TagLoader.EntryWithSource>> tagMap) {
 		Conductance.MATERIALS.getItemTable().rowMap().forEach((material, map) -> map.forEach((handler, item) -> {
-			if (handler == null || item == null) {
+			if (handler == null || item == null || material == null) {
 				return;
 			}
-			final List<TagKey<Item>> entryTags = handler.getEntryTags(BuiltInRegistries.ITEM, material);
-			for (final TagKey<Item> entryTag : entryTags) {
+			for (final TagKey<Item> entryTag : handler.getEntryTags(BuiltInRegistries.ITEM, material)) {
 				tagMap.computeIfAbsent(entryTag.location(), k -> new ArrayList<>())
-						.add(new TagLoader.EntryWithSource(TagEntry.element(BuiltInRegistries.ITEM.getKey(item)), TagGenerationHandler.TAG_SOURCE));
+					.add(new TagLoader.EntryWithSource(TagEntry.element(BuiltInRegistries.ITEM.getKey(item)), TagGenerationHandler.TAG_SOURCE));
 			}
-			for (final TagKey<Item> groupTag : handler.getGroupTags(BuiltInRegistries.ITEM, material)) {
-				final List<TagLoader.EntryWithSource> list = tagMap.computeIfAbsent(groupTag.location(), k -> new ArrayList<>());
-				for (final TagKey<Item> entryTag : entryTags) {
-					list.add(new TagLoader.EntryWithSource(TagEntry.tag(entryTag.location()), TagGenerationHandler.TAG_SOURCE));
-				}
+			for (final TagKey<Item> entryTag : handler.getGroupTags(BuiltInRegistries.ITEM, material)) {
+				tagMap.computeIfAbsent(entryTag.location(), k -> new ArrayList<>())
+					.add(new TagLoader.EntryWithSource(TagEntry.element(BuiltInRegistries.ITEM.getKey(item)), TagGenerationHandler.TAG_SOURCE));
 			}
 		}));
 		Conductance.MATERIALS.getBlockTable().rowMap().forEach((material, map) -> map.forEach((handler, block) -> {
-			if (handler == null || block == null) {
+			if (handler == null || block == null || material == null) {
 				return;
 			}
-			final List<TagKey<Item>> entryTags = handler.getEntryTags(BuiltInRegistries.ITEM, material);
-			for (final TagKey<Item> entryTag : entryTags) {
+			for (final TagKey<Item> entryTag : handler.getEntryTags(BuiltInRegistries.ITEM, material)) {
 				tagMap.computeIfAbsent(entryTag.location(), k -> new ArrayList<>())
-						.add(new TagLoader.EntryWithSource(TagEntry.element(BuiltInRegistries.BLOCK.getKey(block)), TagGenerationHandler.TAG_SOURCE));
+					.add(new TagLoader.EntryWithSource(TagEntry.element(BuiltInRegistries.BLOCK.getKey(block)), TagGenerationHandler.TAG_SOURCE));
 			}
 			for (final TagKey<Item> groupTag : handler.getGroupTags(BuiltInRegistries.ITEM, material)) {
-				final List<TagLoader.EntryWithSource> list = tagMap.computeIfAbsent(groupTag.location(), k -> new ArrayList<>());
-				for (final TagKey<Item> entryTag : entryTags) {
-					list.add(new TagLoader.EntryWithSource(TagEntry.tag(entryTag.location()), TagGenerationHandler.TAG_SOURCE));
-				}
+				tagMap.computeIfAbsent(groupTag.location(), k -> new ArrayList<>())
+					.add(new TagLoader.EntryWithSource(TagEntry.element(BuiltInRegistries.BLOCK.getKey(block)), TagGenerationHandler.TAG_SOURCE));
 			}
 		}));
 		TagGenerationHandler.CUSTOM_ITEM_TAGS.forEach((tagKey, items) -> {
@@ -93,26 +87,23 @@ final class TagGenerationHandler {
 		//			}
 		//		});
 		Conductance.MATERIALS.getBlockTable().rowMap().forEach((material, map) -> map.forEach((handler, block) -> {
-			if (handler == null || block == null) {
+			if (handler == null || block == null || material == null) {
 				return;
 			}
 			final ResourceLocation blockId = BuiltInRegistries.BLOCK.getKey(block);
-			final List<TagKey<Block>> entryTags = handler.getEntryTags(BuiltInRegistries.BLOCK, material);
-			for (final TagKey<Block> entryTag : entryTags) {
+			for (final TagKey<Block> entryTag : handler.getEntryTags(BuiltInRegistries.BLOCK, material)) {
 				tagMap.computeIfAbsent(entryTag.location(), k -> new ArrayList<>())
-						.add(new TagLoader.EntryWithSource(TagEntry.element(blockId), TagGenerationHandler.TAG_SOURCE));
+					.add(new TagLoader.EntryWithSource(TagEntry.element(blockId), TagGenerationHandler.TAG_SOURCE));
 			}
-			for (final TagKey<Block> groupTag : handler.getGroupTags(BuiltInRegistries.BLOCK, material)) {
-				final List<TagLoader.EntryWithSource> list = tagMap.computeIfAbsent(groupTag.location(), k -> new ArrayList<>());
-				for (final TagKey<Block> entryTag : entryTags) {
-					list.add(new TagLoader.EntryWithSource(TagEntry.tag(entryTag.location()), TagGenerationHandler.TAG_SOURCE));
-				}
+			for (final TagKey<Block> entryTag : handler.getGroupTags(BuiltInRegistries.BLOCK, material)) {
+				tagMap.computeIfAbsent(entryTag.location(), k -> new ArrayList<>())
+					.add(new TagLoader.EntryWithSource(TagEntry.element(blockId), TagGenerationHandler.TAG_SOURCE));
 			}
 			tagMap.computeIfAbsent(material.getProp(NCMaterialProps.REQUIRED_TOOL_LEVEL, BlockTags.NEEDS_STONE_TOOL).location(), k -> new ArrayList<>())
-					.add(new TagLoader.EntryWithSource(TagEntry.element(blockId), TagGenerationHandler.TAG_SOURCE));
+				.add(new TagLoader.EntryWithSource(TagEntry.element(blockId), TagGenerationHandler.TAG_SOURCE));
 			handler.getMiningToolTypeTags().forEach(tagKey -> {
 				tagMap.computeIfAbsent(tagKey.location(), k -> new ArrayList<>())
-						.add(new TagLoader.EntryWithSource(TagEntry.element(blockId), TagGenerationHandler.TAG_SOURCE));
+					.add(new TagLoader.EntryWithSource(TagEntry.element(blockId), TagGenerationHandler.TAG_SOURCE));
 			});
 		}));
 	}
