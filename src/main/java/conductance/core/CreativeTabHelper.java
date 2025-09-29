75e32b0e48d2fe96ba7537991b35b460d039726c
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-09-29 18:04:13 +0200

    Change some creative tab icons

--- src/main/java/conductance/core/CreativeTabHelper.java
+++ src/main/java/conductance/core/CreativeTabHelper.java
@@ -8,9 +8,11 @@ import java.util.function.Supplier;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
+import com.mojang.datafixers.util.Either;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import conductance.api.CAPI;
+import conductance.api.NCItems;
 import conductance.api.NCMaterialGenerationHandlers;
 import conductance.api.NCMaterials;
 
@@ -18,22 +20,26 @@ public final class CreativeTabHelper {
 
 	@RequiredArgsConstructor
 	public enum Tabs {
-		GENERAL(Items.IRON_INGOT::getDefaultInstance),
+		GENERAL(() -> NCItems.WRENCH.value().getDefaultInstance()),
 		MACHINE(Items.FURNACE::getDefaultInstance),
 		MATERIAL(() -> CAPI.materials().getItem(NCMaterials.ALUMINIUM, NCMaterialGenerationHandlers.INGOT, 1)),
-		ORE(Items.DIAMOND_ORE::getDefaultInstance);
+		ORE(() -> CAPI.materials().getItem(NCMaterials.ALUMINIUM, NCMaterialGenerationHandlers.ORE_STONE, 1));
 
 		private final @Getter String name = super.toString().toLowerCase(Locale.ROOT);
 		private final @Getter Supplier<ItemStack> icon;
 	}
 
-	private static final EnumMap<Tabs, List<ItemLike>> CONTENT_MAPPING = new EnumMap<>(Tabs.class);
+	private static final EnumMap<Tabs, List<Either<ItemLike, ItemStack>>> CONTENT_MAPPING = new EnumMap<>(Tabs.class);
 
 	public static void addToTab(final ItemLike item, final Tabs tab) {
-		CreativeTabHelper.CONTENT_MAPPING.computeIfAbsent(tab, k -> new ArrayList<>()).add(item);
+		CreativeTabHelper.CONTENT_MAPPING.computeIfAbsent(tab, k -> new ArrayList<>()).add(Either.left(item));
 	}
 
-	public static List<ItemLike> getTabContent(final Tabs tab) {
+	public static void addToTab(final ItemStack stack, final Tabs tab) {
+		CreativeTabHelper.CONTENT_MAPPING.computeIfAbsent(tab, k -> new ArrayList<>()).add(Either.right(stack));
+	}
+
+	public static List<Either<ItemLike, ItemStack>> getTabContent(final Tabs tab) {
 		return CreativeTabHelper.CONTENT_MAPPING.getOrDefault(tab, List.of());
 	}
 
