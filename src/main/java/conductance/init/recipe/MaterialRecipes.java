7a1d50b5870e90c09b59cdd30f432d5718b541c9
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-09-29 14:16:02 +0200

    Add pulverizer recipes for all basic material items

--- src/main/java/conductance/init/recipe/MaterialRecipes.java
+++ src/main/java/conductance/init/recipe/MaterialRecipes.java
@@ -20,6 +20,9 @@ import static conductance.api.NCMaterialGenerationHandlers.FRAME_BOX;
 import static conductance.api.NCMaterialGenerationHandlers.GEAR;
 import static conductance.api.NCMaterialGenerationHandlers.GEAR_SMALL;
 import static conductance.api.NCMaterialGenerationHandlers.GEM;
+import static conductance.api.NCMaterialGenerationHandlers.GEM_EXQUISITE;
+import static conductance.api.NCMaterialGenerationHandlers.GEM_FLAWED;
+import static conductance.api.NCMaterialGenerationHandlers.GEM_FLAWLESS;
 import static conductance.api.NCMaterialGenerationHandlers.INGOT;
 import static conductance.api.NCMaterialGenerationHandlers.NUGGET;
 import static conductance.api.NCMaterialGenerationHandlers.PLATE;
@@ -66,6 +69,46 @@ final class MaterialRecipes {
 				});
 			}
 		}
+		if (INGOT.test(material)) {
+			if (DUST.test(material)) {
+				calc(material, INGOT, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_ingot".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, INGOT, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
+		}
+		if (GEM.test(material)) {
+			if (DUST.test(material)) {
+				calc(material, GEM, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_gem".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, GEM, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
+		}
+		if (GEM_FLAWED.test(material)) {
+			if (DUST.test(material)) {
+				calc(material, GEM_FLAWED, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_flawed_gem".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, GEM_FLAWED, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
+		}
+		if (GEM_FLAWLESS.test(material)) {
+			if (DUST.test(material)) {
+				calc(material, GEM_FLAWLESS, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_flawless_gem".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, GEM_FLAWLESS, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
+		}
+		if (GEM_EXQUISITE.test(material)) {
+			if (DUST.test(material)) {
+				calc(material, GEM_EXQUISITE, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_exquisite_gem".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, GEM_EXQUISITE, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
+		}
 		if (NUGGET.test(material)) {
 			if (!materials().hasItemOverride(material, NUGGET)) {
 				if (material.hasFlag(NCMaterialFlags.INGOT)) {
@@ -74,6 +117,12 @@ final class MaterialRecipes {
 					event.shapeless("%s_ingot_from_nuggets".formatted(material.getName()), materials().getItem(material, INGOT),
 						b -> b.add(materials().getItemTag(material, NUGGET), 9));
 				}
+			}
+			if (DUST.test(material)) {
+				calc(material, NUGGET, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_nugget".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, NUGGET, inAmount).out(material, DUST, outAmount).duration(time));
+				});
 			}
 		}
 		if (PLATE.test(material)) {
@@ -85,12 +134,24 @@ final class MaterialRecipes {
 			}
 			//Gem <-> plate will be handles by cutting machine later
 			//Dust <-> plate will be handles by compressor machine later
+			if (DUST.test(material)) {
+				calc(material, PLATE, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_plate".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, PLATE, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
 		}
 		if (GEAR.test(material)) {
 			//TODO ingot -> extruder
 			if (PLATE.test(material) && ROD.test(material)) {
 				event.shaped("%s_gear".formatted(material.getName()), materials().getItem(material, GEAR),
 					b -> b.pattern("aba", "bWb", "aba").key('a', ROD, material).key('b', PLATE, material));
+			}
+			if (DUST.test(material)) {
+				calc(material, GEAR, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_gear".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, GEAR, inAmount).out(material, DUST, outAmount).duration(time));
+				});
 			}
 		}
 		if (GEAR_SMALL.test(material)) {
@@ -99,12 +160,24 @@ final class MaterialRecipes {
 				event.shaped("small_%s_gear".formatted(material.getName()), materials().getItem(material, GEAR_SMALL),
 					b -> b.pattern(" a ", "XbH", " a ").key('a', ROD, material).key('b', PLATE, material));
 			}
+			if (DUST.test(material)) {
+				calc(material, GEAR_SMALL, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_small_gearblock".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, GEAR_SMALL, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
 		}
 		if (BOLT.test(material)) {
 			//TODO ingot -> extruder
 			if (ROD.test(material)) {
 				event.shapeless("%s_bolt".formatted(material.getName()), materials().getItem(material, BOLT, 2),
 					b -> b.add(TAG_WIRE_CUTTERS).add(ROD, material));
+			}
+			if (DUST.test(material)) {
+				calc(material, BOLT, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_bolt".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, BOLT, inAmount).out(material, DUST, outAmount).duration(time));
+				});
 			}
 		}
 		if (SCREW.test(material)) {
@@ -113,12 +186,24 @@ final class MaterialRecipes {
 				event.shapeless("%s_screw".formatted(material.getName()), materials().getItem(material, SCREW),
 					b -> b.add(TAG_HAMMERS).add(BOLT, material));
 			}
+			if (DUST.test(material)) {
+				calc(material, SCREW, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_screw".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, SCREW, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
 		}
 		if (FOIL.test(material)) {
 			//TODO ingot -> extruder
 			if (PLATE.test(material)) {
 				event.shapeless("%s_foil".formatted(material.getName()), materials().getItem(material, FOIL),
 					b -> b.add(TAG_HAMMERS).add(PLATE, material));
+			}
+			if (DUST.test(material)) {
+				calc(material, FOIL, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_foil".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, FOIL, inAmount).out(material, DUST, outAmount).duration(time));
+				});
 			}
 		}
 		if (RING.test(material)) {
@@ -127,12 +212,24 @@ final class MaterialRecipes {
 				event.shapeless("%s_ring".formatted(material.getName()), materials().getItem(material, RING),
 					b -> b.add(TAG_HAMMERS).add(ROD, material));
 			}
+			if (DUST.test(material)) {
+				calc(material, RING, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_ring".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, RING, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
 		}
 		if (ROTOR.test(material)) {
 			//TODO ingot -> extruder
 			if (PLATE.test(material) && RING.test(material) && SCREW.test(material)) {
 				event.shaped("%s_rotor".formatted(material.getName()), materials().getItem(material, ROTOR),
 					b -> b.pattern("aWa", "bcH", "aXa").key('a', PLATE, material).key('b', SCREW, material).key('c', RING, material));
+			}
+			if (DUST.test(material)) {
+				calc(material, ROTOR, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_rotor".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, ROTOR, inAmount).out(material, DUST, outAmount).duration(time));
+				});
 			}
 		}
 		if (FINE_WIRE.test(material)) {
@@ -141,11 +238,23 @@ final class MaterialRecipes {
 				event.shapeless("fine_%s_wire".formatted(material.getName()), materials().getItem(material, FINE_WIRE),
 					b -> b.add(TAG_WIRE_CUTTERS).add(FOIL, material));
 			}
+			if (DUST.test(material)) {
+				calc(material, FINE_WIRE, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_fine_wire".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, FINE_WIRE, inAmount).out(material, DUST, outAmount).duration(time));
+				});
+			}
 		}
 		if (FRAME_BOX.test(material)) {
 			if (ROD.test(material)) {
 				event.shaped("%s_frame_box".formatted(material.getName()), materials().getItem(material, FRAME_BOX, 2),
 					b -> b.pattern("aaa", "aWa", "aaa").key('a', ROD, material));
+			}
+			if (DUST.test(material)) {
+				calc(material, FRAME_BOX, DUST, (int) material.getMass(), (inAmount, outAmount, time) -> {
+					event.create("%s_dust_from_frame_box".formatted(material.getName()), NCRecipeTypes.PULVERIZER,
+						b -> b.in(material, FRAME_BOX, inAmount).out(material, DUST, outAmount).duration(time));
+				});
 			}
 		}
 	}
