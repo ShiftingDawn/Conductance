9658dddfa16dc9e16785a1093279046a567811d9
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-10-10 23:59:39 +0200

    Prevent auto output on front of machine

--- src/main/java/conductance/lib/GridInteractionHandler.java
+++ src/main/java/conductance/lib/GridInteractionHandler.java
@@ -6,6 +6,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.IEventBus;
@@ -14,6 +15,10 @@ import conductance.api.block.FacingAndRotation;
 import conductance.api.block.GridInteractionHelper;
 import conductance.api.block.IGridInteractable;
 import conductance.api.block.InteractType;
+import conductance.api.machine.IFluidAutoOutput;
+import conductance.api.machine.IItemAutoOutput;
+import conductance.api.machine.MachineBlockEntity;
+import conductance.api.machine.MachineCapability;
 import static conductance.api.NCBlockStateProperties.FACING_ALL;
 import static conductance.api.NCBlockStateProperties.FACING_EXTENDED;
 import static conductance.api.NCBlockStateProperties.FACING_HORIZONTAL;
@@ -29,7 +34,7 @@ public final class GridInteractionHandler {
 		final UseOnContext ctx = new UseOnContext(event.getLevel(), event.getEntity(), event.getHand(), event.getItemStack(), event.getHitVec());
 		final Direction side = GridInteractionHelper.getLogicalSideFromGrid(event.getHitVec());
 		if (GridInteractionHelper.shouldInteractUsingGrid(ctx)) {
-			final InteractionResult result = GridInteractionHandler.handleExtendedInteraction(ctx, side);
+			final InteractionResult result = GridInteractionHandler.handleGridInteraction(ctx, side);
 			if (result.consumesAction()) {
 				event.setCanceled(true);
 				event.setCancellationResult(result);
@@ -37,7 +42,7 @@ public final class GridInteractionHandler {
 		}
 	}
 
-	private static InteractionResult handleExtendedInteraction(final UseOnContext ctx, final Direction side) {
+	private static InteractionResult handleGridInteraction(final UseOnContext ctx, final Direction side) {
 		final InteractType interactType = InteractType.findTypeForStack(ctx.getItemInHand());
 		final BlockEntity blockEntity = ctx.getLevel().getBlockEntity(ctx.getClickedPos());
 		if (interactType != null) {
@@ -73,7 +78,10 @@ public final class GridInteractionHandler {
 		final BlockPos pos = ctx.getClickedPos();
 		final Level level = ctx.getLevel();
 		final BlockState state = level.getBlockState(pos);
-		if (player != null && player.isCrouching()) {
+		if (player == null) {
+			return false;
+		}
+		if (player.isCrouching()) {
 			if (state.hasProperty(FACING_ALL) && side != state.getValue(FACING_ALL)) {
 				level.setBlockAndUpdate(pos, state.setValue(FACING_ALL, side));
 				return true;
@@ -90,6 +98,31 @@ public final class GridInteractionHandler {
 				final FacingAndRotation current = state.getValue(FACING_EXTENDED);
 				if (side != current.getFacing()) {
 					level.setBlockAndUpdate(pos, state.setValue(FACING_EXTENDED, FacingAndRotation.get(side, current.getRotation())));
+					return true;
+				}
+			}
+		} else {
+			if (state.hasProperty(FACING_EXTENDED)) {
+				final FacingAndRotation current = state.getValue(FACING_EXTENDED);
+				if (side == current.getFacing()) {
+					final Rotation rotation = Rotation.values()[(current.getRotation().ordinal() + 1) % Rotation.values().length];
+					level.setBlockAndUpdate(pos, state.setValue(FACING_EXTENDED, FacingAndRotation.get(side, rotation)));
+					return true;
+				}
+			}
+			if (level.getBlockEntity(pos) instanceof final MachineBlockEntity<?> machine) {
+				boolean hasChanged = false;
+				for (final MachineCapability capability : machine.getCapabilities().values()) {
+					if (capability instanceof final IItemAutoOutput itemAutoOutput && side != machine.getFacing()) {
+						itemAutoOutput.setItemAutoOutputSide(side);
+						hasChanged = true;
+					}
+					if (capability instanceof final IFluidAutoOutput fluidAutoOutput && side != machine.getFacing()) {
+						fluidAutoOutput.setFluidAutoOutputSide(side);
+						hasChanged = true;
+					}
+				}
+				if (hasChanged) {
 					return true;
 				}
 			}
