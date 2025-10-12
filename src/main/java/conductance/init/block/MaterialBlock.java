e6867301c5fbbc6b38b812f5c5edc5e618a96716
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-10-12 21:25:16 +0200

    Partially transparent material blocks (like frame boxes) wont render sides next themselves like glass

--- src/main/java/conductance/init/block/MaterialBlock.java
+++ src/main/java/conductance/init/block/MaterialBlock.java
@@ -1,11 +1,13 @@
 package conductance.init.block;
 
 import java.util.List;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import lombok.Getter;
 import conductance.api.NCMaterialProps;
 import conductance.api.block.IGeneratedMiningTags;
@@ -24,6 +26,16 @@ public final class MaterialBlock extends Block implements IMaterialBlock, IGener
 		this.material = material;
 		this.handler = handler;
 		this.name = Component.translatable(handler.makeDescriptionId(material), Component.translatable(material.getDescriptionId()));
+	}
+
+	@Override
+	protected boolean skipRendering(final BlockState state, final BlockState adjacentState, final Direction direction) {
+		if (!this.handler.shouldOccludeBlocks()) {
+			if (adjacentState.getBlock() instanceof final IMaterialBlock materialBlock && materialBlock.getHandler() == this.handler) {
+				return true;
+			}
+		}
+		return super.skipRendering(state, adjacentState, direction);
 	}
 
 	@Override
