c20297fcfe12911b1c099bf252e029590407fad1
ShiftingDawn <191372323+ShiftingDawn@users.noreply.github.com>
2025-10-13 22:06:44 +0200

    Add program circuit recipe

--- src/main/java/conductance/init/recipe/ConductanceRecipeLoader.java
+++ src/main/java/conductance/init/recipe/ConductanceRecipeLoader.java
@@ -4,11 +4,14 @@ import net.neoforged.neoforge.common.Tags;
 import conductance.api.NCItems;
 import conductance.api.NCMaterialGenerationHandlers;
 import conductance.api.NCMaterials;
+import conductance.api.NCTiers;
 import conductance.api.plugin.ConductancePluginListener;
 import conductance.api.plugin.EventListener;
 import conductance.api.recipe.event.RegisterRecipeEvent;
+import conductance.api.tier.TieredItemType;
 import conductance.api.util.ExtruderShape;
 import conductance.Conductance;
+import conductance.init.item.ProgramCircuitItem;
 
 @ConductancePluginListener(modid = Conductance.MODID)
 final class ConductanceRecipeLoader {
@@ -22,6 +25,7 @@ final class ConductanceRecipeLoader {
 			.pattern("aHa", "aaa", " a ").key('a', NCMaterialGenerationHandlers.PLATE, NCMaterials.COPPER));
 		event.shaped(NCItems.WIRE_CUTTERS, b -> b
 			.pattern("aHa", "bab", "cWc").key('a', NCMaterialGenerationHandlers.PLATE, NCMaterials.COPPER).key('b', Tags.Items.STRINGS).key('c', NCMaterialGenerationHandlers.ROD, NCMaterials.WOOD));
+		event.shapeless(ProgramCircuitItem.makeStack(0), b -> b.add(NCItems.TIERED.get(TieredItemType.CIRCUIT, NCTiers.LV)));
 
 		MaterialRecipes.add(event);
 		TierRecipes.add(event);
